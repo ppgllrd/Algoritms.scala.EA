e53f46bbc124e678aea2622add1490e5f1d0accd
ppgllrd <ppgllrd@gmail.com>
2016-03-23 18:14:56 +0100

    Use real CPU time instead of wall clock time in Timer.

--- src/EA/util/Timer.scala
+++ src/EA/util/Timer.scala
@@ -8,20 +8,34 @@
 
 package EA.util
 
+import java.lang.management.ManagementFactory
+
 case class Timer() {
   import EA.Seconds
 
-  private var startTime = System.currentTimeMillis()
+  private val nanosecond = 1000000000.0
+
+  private val bean = ManagementFactory.getThreadMXBean()
+  if(!bean.isCurrentThreadCpuTimeSupported())
+    sys.error("isCurrentThreadCpuTimeSupported not supported")
+
+  // Only valid for single threaded applications
+  // see: http://nadeausoftware.com/articles/2008/03/java_tip_how_get_cpu_and_user_time_benchmarking#TimingasinglethreadedtaskusingCPUsystemandusertime
+
+  private def getCpuTime() =
+    bean.getCurrentThreadCpuTime()
+
+  private var startTime = getCpuTime() // System.currentTimeMillis()
 
   // resets start of timer to current time
   def reset(): Unit = {
-    startTime = System.currentTimeMillis()
+    startTime = getCpuTime() // System.currentTimeMillis()
   }
 
   // returns elapsed time (in seconds) since last reset
   def elapsedTime() : Seconds = {
-    val t = System.currentTimeMillis()
-    val segs = (t - startTime).toDouble / 1000.0
+    val t = getCpuTime() // System.currentTimeMillis()
+    val segs = (t - startTime).toDouble / nanosecond
     segs
   }
 }
